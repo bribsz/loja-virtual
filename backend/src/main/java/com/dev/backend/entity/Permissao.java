b2f40fab36dc9371a8bce54433f87651c99c6b80
Breno Ribeiro <breno.cano@uniriotec.br>
2023-02-17 16:00:25 -0300

    crud produtos-permissao

---
+++ backend/src/main/java/com/dev/backend/entity/Permissao.java
@@ -0,0 +1,54 @@
+package com.dev.backend.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Table(name = "permissao")
+@Data
+public class Permissao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String nome;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCriacao;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataAtualizacao;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+}
