09a96c954c35496c7b4bdbe03fec7da646d39d8e
Breno Ribeiro <breno.cano@uniriotec.br>
2023-03-07 22:55:59 -0300

    updates

--- backend/src/main/java/com/dev/backend/service/PessoaClienteService.java
+++ backend/src/main/java/com/dev/backend/service/PessoaClienteService.java
@@ -1,6 +1,8 @@
 package com.dev.backend.service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,8 +28,11 @@ public class PessoaClienteService {
 		pessoa.setDataCriacao(new Date());
 		Pessoa pessoaNova = pessoaRepository.saveAndFlush(pessoa);
 		permissaoPessoaService.vincularPessoaPermissaoCliente(pessoaNova);
-		emailService.enviarEmailTexto(pessoaNova.getEmail(), "Cadastro na Loja xxx", "O registro foi com suscesso");
-		
+		//emailService.enviarEmailTexto(pessoaNova.getEmail(), "Cadastro na Loja xxx", "O registro foi com suscesso");
+		Map<String, Object> proprMap = new HashMap<>();
+		proprMap.put("nome", pessoaNova.getNome());
+		proprMap.put("mensagem", "O registro foi com suscesso");
+		emailService.enviarEmailTemplate(pessoaNova.getEmail(), "Cadastro na Loja x", proprMap);
 		return pessoaNova;
 	}
 	
